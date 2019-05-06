ac3fb118e19129899a09084f34e8448d73b3f51a
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-06 18:46:19 -0500

    Adds the styling of the metronome controls, and the .wav files

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 import './Metronome.css';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 
 class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+  }
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({ bpm });
+  }
+
   render() {
-    return <div className="metronome">hi</div>;
+    const { playing, bpm } = this.state;
+
+    return (
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input 
+            type="range" 
+            min="60" 
+            max="240" 
+            value={bpm} 
+            onChange={this.handleBpmChange}
+            />
+        </div>
+        <button>{playing ? 'Stop' : 'Start'}</button>
+      </div>
+    );
   }
 }
 
